b7adcd743b7c689b0c897c21c78891de6006c56a
arya-bhanu <aryacreative.dev@gmail.com>
2025-01-09 10:28:54 +0700

    feat: components/selectors --done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonBasicComponent } from './button-basic/button-basic.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonBasicComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'learn-angular-2';
